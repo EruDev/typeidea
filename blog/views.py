008a119733becbb1485a84b57a54835d24cc1ff9
zhaopengfei <1027926875@qq.com>
2018-12-22 11:41:00 +0800

    分类导航

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage
 
 from blog.models import Post, Tag, Category
+from config.models import SideBar
+from comment.models import Comment
 
 
 def post_list(request, category_id=None, tag_id=None):
@@ -27,8 +29,26 @@ def post_list(request, category_id=None, tag_id=None):
         posts = paginator.page(cur_page)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
+    categories = Category.objects.filter(status=1)
+    nav_cates = []
+    cates = []
+    for cate in categories:
+        if cate.is_nav:
+            nav_cates.append(cate)
+        else:
+            cates.append(cate)
+    recently_comments = Comment.objects.filter(status=1)[:10]
+    recently_sidebars = SideBar.objects.filter(status=1)
+    recently_categories = Post.objects.filter(status=1)[:10]
 
-    context = {'posts': posts}
+    context = {
+        'posts': posts,
+        'nav_cates': nav_cates,
+        'cates': cates,
+        'recently_categories': recently_categories,
+        'recently_sidebars': recently_sidebars,
+        'recently_comments': recently_comments
+    }
     return render(request, 'blog/post.html', context)
 
 
